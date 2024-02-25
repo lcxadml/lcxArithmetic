58ca50671aa67924622e7e5190e281d4fa21aaa4
licunxing <864255598@qq.com>
2024-02-25 18:57:31 +0800

    feat: 合并两个有序数组

---
+++ 88.合并两个有序数组/index.js
@@ -0,0 +1,11 @@
+const nums1 = [1, 2, 3, 0, 0, 0],
+  m = 3,
+  nums2 = [2, 5, 6],
+  n = 3;
+
+const merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, n, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
+
+merge(nums1, m, nums2, n);
