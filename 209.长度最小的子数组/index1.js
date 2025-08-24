93f12161f7538a3ae761fe89746dae08d533849e
licunxing <864255598@qq.com>
2025-08-24 20:15:50 +0800

    feat: 长度最小数组（滑动窗口泻法）

--- 209.长度最小的子数组/index1.js
+++ 209.长度最小的子数组/index1.js
@@ -4,30 +4,33 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
+  let arr = [],
     count = 0,
     res = 99999999;
-  let m = 0;
-
   for (let i = 0; i < nums.length; i++) {
+    arr.push(nums[i]);
     count += nums[i];
-
     if (count < target) {
       continue;
     } else {
-      while (count >= target) {
-        count -= nums[m];
-        m++;
-        console.log(m, i);
+      const len = arr.length;
+      let j = i - len;
+      for (; count >= target; j++) {
+        count -= nums[j];
       }
+      // while (count >= target) {
+      //   count -= arr[0];
+      //   arr.shift();
+      // }
 
-      res = Math.min(i + 2 - m, res);
+      res = Math.min(i - j, res);
     }
   }
 
   return res === 99999999 ? 0 : res;
 };
 
-const target = 11,
-  nums = [1, 1, 1, 1, 1, 1, 1, 1];
+const target = 7,
+  nums = [2, 3, 1, 2, 4, 3];
 
 console.log(minSubArrayLen(target, nums));
